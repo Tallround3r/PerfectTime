5f9fececfc61e57bd6e28634045a198e2c21ee00
Lukas Karcher (TP) <l.karcher@t-online.de>
2018-10-12 10:38:24 +0200

    firebase api key outsourced

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-	apiKey: 'AIzaSyBU77VjjQMQFFU1XHpNrlpZa5WjLz2yG7k',
-	authDomain: 'perfecttime-planyourtrip.firebaseapp.com',
-	databaseURL: 'https://perfecttime-planyourtrip.firebaseio.com',
-	projectId: 'perfecttime-planyourtrip',
-	storageBucket: 'perfecttime-planyourtrip.appspot.com',
-	messagingSenderId: '998140555209',
-};
