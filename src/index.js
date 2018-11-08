a191333892fdb9b7aaa46e7b75f44154fd1460d6
Lukas Karcher <l.karcher@t-online.de>
2018-11-08 11:22:36 +0100

    fixed connection to firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+import dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import dotenv from 'dotenv';
 
 dotenv.config();
 
