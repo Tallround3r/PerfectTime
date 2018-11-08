b1fef861fddd82deac8ee4ebb62281e0a5f41eb7
Lukas Karcher <l.karcher@t-online.de>
2018-11-08 09:58:13 +0100

    installed dotenv; addded .env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,13 @@
 import * as firebase from 'firebase';
-import {config} from '../../firebase-config';
 
+const config = {
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	databaseURL: process.env.DATABASE_URL,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+};
 
 if (!firebase.apps.length) {
 	firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
