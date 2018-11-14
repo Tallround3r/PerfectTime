96dc0844b5a9a31595ccba4407090a582ff699bd
Lukas Karcher <l.karcher@t-online.de>
2018-11-14 11:35:44 +0100

    merge errors fixed

--- src/container/LocationsPage.js
+++ src/container/LocationsPage.js
@@ -6,11 +6,9 @@ import {withStyles} from '@material-ui/core';
 import * as routes from '../constants/routes';
 import {NavLink} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import Avatar from '@material-ui/core/Avatar';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import {withStyles} from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import StarIcon from '@material-ui/icons/Star';
 import AddIcon from '@material-ui/icons/Add';
@@ -19,25 +17,25 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 
 const styles = theme => ({
-    borderAround: {
-        border: "thin solid #000000"
-    },
-    bigColumn: {
-        flexBasis: '50%',
-        textAlign: 'center',
-    },
-    smallColumn: {
-        flexBasis: '1%',
-        textAlign: 'right',
-    },
-    verticalLine: {
-        borderLeft: "thin solid #000000",
-        height: '100px',
-    },
-    hrLine: {
-        width: '1px',
-        height: '67px',
-    },
+	borderAround: {
+		border: 'thin solid #000000',
+	},
+	bigColumn: {
+		flexBasis: '50%',
+		textAlign: 'center',
+	},
+	smallColumn: {
+		flexBasis: '1%',
+		textAlign: 'right',
+	},
+	verticalLine: {
+		borderLeft: 'thin solid #000000',
+		height: '100px',
+	},
+	hrLine: {
+		width: '1px',
+		height: '67px',
+	},
 });
 
 class LocationsPage extends React.Component {
@@ -62,99 +60,102 @@ class LocationsPage extends React.Component {
 						: isEmpty(locations)
 							? 'No Locations created yet.'
 							: Object.keys(locations).map((key, index) => {
-								let date = new Date(locations[key].startdate.seconds * 1000)
-                return(
-                                <ExpansionPanel className={classes.borderAround} key={label}>
-                                    <ExpansionPanelSummary>
-                                        <div className={classes.smallColumn}>
-                                            <Avatar><StarIcon/></Avatar>
-                                        </div>
-                                        <div className={classes.bigColumn}>
-                                            <Typography>{label}</Typography>
-                                        </div>
-                                        <div className={classes.bigColumn}>
-                                            <Typography>{label}</Typography>
-                                        </div>
-                                        <div className={classes.bigColumn}>
-                                            <Typography>{label}</Typography>
-                                        </div>
-                                        <div className={classes.smallColumn}>
-                                            <NavLink exact to={routes.LOCATIONS_EDIT}><Avatar><ArrowRightIcon/></Avatar></NavLink>
-                                        </div>
-                                    </ExpansionPanelSummary>
-                                    <ExpansionPanelDetails>
-                                        <GridList cols={37} cellHeight={"auto"} className={classes.verticalLine}>
-                                            <GridListTile></GridListTile>
-                                            <GridListTile>
-                                                <hr className={classes.hrLine}/>
-                                            </GridListTile>
-                                            <GridListTile style={{width: 'auto'}}>
-                                                <br/>
-                                                <NavLink exact to={routes.LOCATIONS_ADD}>
-                                                    <Avatar><AddIcon/></Avatar>
-                                                </NavLink>
-                                            </GridListTile>
-                                        </GridList>
-                                    </ExpansionPanelDetails>
-                                </ExpansionPanel>
-                    );
+								let date = new Date(locations[key].startdate.seconds * 1000);
+								let label = locations[key].title;
+								return (
+									<ExpansionPanel className={classes.borderAround} key={label}>
+										<ExpansionPanelSummary>
+											<div className={classes.smallColumn}>
+												<Avatar><StarIcon/></Avatar>
+											</div>
+											<div className={classes.bigColumn}>
+												<Typography>{label}</Typography>
+											</div>
+											<div className={classes.bigColumn}>
+												<Typography>{label}</Typography>
+											</div>
+											<div className={classes.bigColumn}>
+												<Typography>{label}</Typography>
+											</div>
+											<div className={classes.smallColumn}>
+												<NavLink exact
+												         to={routes.LOCATIONS_EDIT}><Avatar><ArrowRightIcon/></Avatar></NavLink>
+											</div>
+										</ExpansionPanelSummary>
+										<ExpansionPanelDetails>
+											<GridList cols={37} cellHeight={'auto'} className={classes.verticalLine}>
+												<GridListTile></GridListTile>
+												<GridListTile>
+													<hr className={classes.hrLine}/>
+												</GridListTile>
+												<GridListTile style={{width: 'auto'}}>
+													<br/>
+													<NavLink exact to={routes.LOCATIONS_ADD}>
+														<Avatar><AddIcon/></Avatar>
+													</NavLink>
+												</GridListTile>
+											</GridList>
+										</ExpansionPanelDetails>
+									</ExpansionPanel>
+								);
 							})}
-                </div>
+				</div>
 
-                <hr/>
-                <br/>
-                  
-                <div>
-                    {!isLoaded(locations)
+				<hr/>
+				<br/>
+
+				<div>
+					{!isLoaded(locations)
 						? 'Loading...'
 						: isEmpty(locations)
 							? 'No Locations created yet.'
 							: Object.keys(locations).map((key, index) => {
-								let date = new Date(locations[key].startdate.seconds * 1000)
-                        return (
-                            <div key={label}>
-                                <ExpansionPanel className={classes.borderAround} key={label}>
-                                    <ExpansionPanelSummary>
-                                        <div className={classes.smallColumn}>
-                                            <Avatar><StarIcon/></Avatar>
-                                        </div>
-                                        <div className={classes.bigColumn}>
-                                            <Typography>{label}</Typography>
-                                        </div>
-                                        <div className={classes.bigColumn}>
-                                            <Typography>{label}</Typography>
-                                        </div>
-                                        <div className={classes.bigColumn}>
-                                            <Typography>{label}</Typography>
-                                        </div>
-                                        <div className={classes.smallColumn}>
-                                            <NavLink exact to={routes.LOCATIONS_EDIT}><Avatar><ArrowRightIcon/></Avatar></NavLink>
-                                        </div>
-                                    </ExpansionPanelSummary>
-                                    <ExpansionPanelDetails>
-                                        <Typography>description</Typography>
-                                    </ExpansionPanelDetails>
-                                </ExpansionPanel>
-                                <GridList cols={37} cellHeight={"auto"}>
-                                    <GridListTile></GridListTile>
-                                    <GridListTile>
-                                        <hr className={classes.hrLine}/>
-                                    </GridListTile>
-                                    <GridListTile style={{width: 'auto'}}>
-                                        <br/>
-                                        <NavLink exact to={routes.LOCATIONS_ADD}>
-                                            <Avatar><AddIcon/></Avatar>
-                                        </NavLink>
-                                    </GridListTile>
-                                </GridList>
-                                <br/>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
-        );
-    }
+								let date = new Date(locations[key].startdate.seconds * 1000);
+								let label = locations[key].title;
+								return (
+									<div key={key}>
+										<ExpansionPanel className={classes.borderAround} key={key}>
+											<ExpansionPanelSummary>
+												<div className={classes.smallColumn}>
+													<Avatar><StarIcon/></Avatar>
+												</div>
+												<div className={classes.bigColumn}>
+													<Typography>{label}</Typography>
+												</div>
+												<div className={classes.bigColumn}>
+													<Typography>{label}</Typography>
+												</div>
+												<div className={classes.bigColumn}>
+													<Typography>{label}</Typography>
+												</div>
+												<div className={classes.smallColumn}>
+													<NavLink exact to={routes.LOCATIONS_EDIT}><Avatar><ArrowRightIcon/></Avatar></NavLink>
+												</div>
+											</ExpansionPanelSummary>
+											<ExpansionPanelDetails>
+												<Typography>{locations[key].description}</Typography>
+											</ExpansionPanelDetails>
+										</ExpansionPanel>
+										<GridList cols={37} cellHeight={'auto'}>
+											<GridListTile></GridListTile>
+											<GridListTile>
+												<hr className={classes.hrLine}/>
+											</GridListTile>
+											<GridListTile style={{width: 'auto'}}>
+												<br/>
+												<NavLink exact to={routes.LOCATIONS_ADD}>
+													<Avatar><AddIcon/></Avatar>
+												</NavLink>
+											</GridListTile>
+										</GridList>
+										<br/>
+									</div>
+								);
+							})}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default compose(
