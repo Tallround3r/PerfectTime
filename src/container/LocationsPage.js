c0b66bf826317bf7425567468429622c923d8046
D070334 <lea-fries@gmx.de>
2018-11-15 13:44:54 +0100

    activities (no backend yet) into LocationsPage with unordered list and vertical line + Chips

--- src/container/LocationsPage.js
+++ src/container/LocationsPage.js
@@ -15,6 +15,7 @@ import AddIcon from '@material-ui/icons/Add';
 import ArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import Chip from '@material-ui/core/Chip';
 
 const styles = theme => ({
     borderAround: {
@@ -28,7 +29,7 @@ const styles = theme => ({
         flexBasis: '1%',
         textAlign: 'right',
     },
-    verticalLine: {
+    borderLeft: {
         borderLeft: 'thin solid #000000',
         height: '100px',
     },
@@ -52,6 +53,8 @@ class LocationsPage extends React.Component {
         return (
             <div>
                 <h1>Locations</h1>
+                {/* Overview with unordered list of activities */}
+                <p><b>Overview with unordered list of activities</b></p>
                 <div>
                     {!isLoaded(locations)
                         ? 'Loading...'
@@ -84,19 +87,95 @@ class LocationsPage extends React.Component {
                                                 </div>
                                             </ExpansionPanelSummary>
                                             <ExpansionPanelDetails>
-                                                <GridList cols={37} cellHeight={'auto'}
-                                                          className={classes.verticalLine}>
-                                                    <GridListTile></GridListTile>
-                                                    <GridListTile>
-                                                        <hr className={classes.hrLine}/>
-                                                    </GridListTile>
-                                                    <GridListTile style={{width: 'auto'}}>
-                                                        <br/>
-                                                        <NavLink exact to={routes.LOCATIONS_ADD}>
-                                                            <Avatar><AddIcon/></Avatar>
-                                                        </NavLink>
-                                                    </GridListTile>
-                                                </GridList>
+                                                <div>
+                                                    <Typography>
+                                                        <ul>
+                                                            <li>Activity 1</li>
+                                                            <li>Activity 2</li>
+                                                            <li>Activity 3</li>
+                                                            <li>Activity 4</li>
+                                                            <li>Activity 5</li>
+                                                            <li>Activity 5</li>
+                                                        </ul>
+                                                    </Typography>
+                                                </div>
+                                            </ExpansionPanelDetails>
+                                        </ExpansionPanel>
+                                        <br/>
+                                    </div>
+                                );
+                            })}
+                </div>
+                <br/>
+                <hr/>
+                <p><b>Overview with vertical line next to activities</b></p>
+                {/* Overview with vertical line next to activities */}
+                <br/>
+                <div>
+                    {!isLoaded(locations)
+                        ? 'Loading...'
+                        : isEmpty(locations)
+                            ? 'No Locations created yet.'
+                            : Object.keys(locations).map((key, index) => {
+                                let label = locations[key].title;
+                                let startdate = new Date(locations[key].startdate.seconds * 1000);
+                                let enddate = new Date(locations[key].enddate.seconds * 1000);
+                                return (
+                                    <div key={key}>
+                                        <ExpansionPanel className={classes.borderAround} key={label}>
+                                            <ExpansionPanelSummary>
+                                                <div className={classes.smallColumn}>
+                                                    <Avatar><StarIcon/></Avatar>
+                                                </div>
+                                                <div className={classes.bigColumn}>
+                                                    <Typography>{label}</Typography>
+                                                </div>
+                                                <div className={classes.bigColumn}>
+                                                    <Typography>Startdate: {startdate.getDate()}.{startdate.getMonth()}.{startdate.getFullYear()}</Typography>
+                                                    <Typography>Enddate: {enddate.getDate()}.{enddate.getMonth()}.{enddate.getFullYear()}</Typography>
+                                                </div>
+                                                <div className={classes.bigColumn}>
+                                                    <Typography>{locations[key].description}</Typography>
+                                                </div>
+                                                <div className={classes.smallColumn}>
+                                                    <NavLink exact
+                                                             to={routes.LOCATIONS_EDIT}><Avatar><ArrowRightIcon/></Avatar></NavLink>
+                                                </div>
+                                            </ExpansionPanelSummary>
+                                            <ExpansionPanelDetails>
+                                                <div className={classes.smallColumn} style={{borderLeft: 'thin solid #000000'}}>
+                                                    <Chip
+                                                        label="Activity 1"
+                                                        component="a"
+                                                        href={routes.ACTIVITY_EDIT}
+                                                        clickable
+                                                        variant="outlined"
+                                                    />
+                                                    <Typography><br/></Typography>
+                                                    <Chip label="Activity 2" variant="outlined"/>
+                                                    <Typography><br/></Typography>
+                                                    <Chip label="Activity 3" variant="outlined"/>
+                                                    <Typography><br/></Typography>
+                                                    <Chip label="Activity 4" variant="outlined"/>
+                                                    <Typography><br/></Typography>
+                                                    <Chip label="Activity 5" variant="outlined"/>
+                                                    <Typography><br/></Typography>
+                                                    <Chip label="Activity 5" variant="outlined"/>
+                                                    <Typography><br/></Typography>
+                                                    <Chip label="Activity 6" variant="outlined"/>
+                                                    <Typography><br/></Typography>
+                                                </div>
+                                                <div className={classes.bigColumn}>
+                                                    <Typography>Startdate Activity 1</Typography>
+                                                    <Typography>Startdate Activity 2</Typography>
+                                                    <Typography>Startdate Activity 3</Typography>
+                                                    <Typography>Startdate Activity 4</Typography>
+                                                    <Typography>Startdate Activity 5</Typography>
+                                                    <Typography>Startdate Activity 6</Typography>
+                                                </div>
+                                                <div className={classes.bigColumn}>
+                                                    <Typography>Hallo</Typography>
+                                                </div>
                                             </ExpansionPanelDetails>
                                         </ExpansionPanel>
                                         <br/>
@@ -105,6 +184,8 @@ class LocationsPage extends React.Component {
                             })}
                 </div>
                 <hr/>
+                <p><b>old version</b></p>
+                {/* old version with hrLine */}
                 <br/>
                 <div>
                     {!isLoaded(locations)
@@ -137,7 +218,11 @@ class LocationsPage extends React.Component {
                                                 </div>
                                             </ExpansionPanelSummary>
                                             <ExpansionPanelDetails>
-                                                <Typography>{locations[key].description}</Typography>
+                                                <Typography>
+                                                    description
+                                                    <br/>
+                                                    description
+                                                </Typography>
                                             </ExpansionPanelDetails>
                                         </ExpansionPanel>
                                         <GridList cols={37} cellHeight={'auto'}>
@@ -173,4 +258,3 @@ export default compose(
     ),
     withStyles(styles),
 )(LocationsPage);
-
