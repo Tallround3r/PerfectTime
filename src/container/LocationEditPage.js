26d2c12529b6a1b6a3abd5f5a2594977c6d48be6
Lukas Karcher <l.karcher@t-online.de>
2018-11-28 12:42:35 +0100

    implemented input fields of LocationPage

--- src/container/LocationEditPage.js
+++ src/container/LocationEditPage.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import {TextField} from '@material-ui/core';
+import {compose} from 'redux';
+import {Button, Paper, TextField, withStyles} from '@material-ui/core';
+import {AddPhotoAlternateOutlined} from '@material-ui/icons';
 import DatePicker from 'material-ui-pickers/DatePicker';
+import {NavLink} from 'react-router-dom';
+import * as routes from '../constants/routes';
+
+
+const styles = theme => ({
+	inputContainer: {
+		display: 'flex',
+		flexDirection: 'column',
+		margin: theme.spacing.unit,
+		paddingRight: theme.spacing.unit * 10,
+	},
+	inputField: {
+		margin: theme.spacing.unit,
+	},
+	imagePaper: {
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: theme.spacing.unit,
+		float: 'right',
+		width: '18em',
+		height: '18em',
+	},
+	imageIcon: {
+		fontSize: '10em',
+	},
+	actionButtonsContainer: {
+		display: 'flex',
+		justifyContent: 'space-evenly',
+		marginTop: theme.spacing.unit * 3,
+	},
+});
 
 const INITIAL_STATE = {
 	title: '',
 	description: '',
 	date: new Date(),
+	address: '',
 };
 
 class LocationEditPage extends React.Component {
@@ -41,44 +76,84 @@ class LocationEditPage extends React.Component {
 	};
 
 	render() {
-		const {title, description, date} = this.state;
+		const {classes} = this.props;
+		const {title, description, date, address} = this.state;
 
 		return (
 			<div>
 				<h1>Edit Location</h1>
 
-				<div>
+				<NavLink exact to={routes.LOCATIONS_EDIT}>
+					<Paper
+						className={classes.imagePaper}
+					>
+						<AddPhotoAlternateOutlined
+							className={classes.imageIcon}
+						/>
+					</Paper>
+				</NavLink>
+
+				<form className={classes.inputContainer}>
 					<TextField
+						className={classes.inputField}
 						label="Title"
 						name="title"
 						value={title}
 						onChange={this.handleChangeInput}
 					/>
 					<TextField
+						className={classes.inputField}
 						label="Description"
 						name="description"
 						value={description}
 						onChange={this.handleChangeInput}
 						multiline
-						rows="4"
 					/>
 					<DatePicker
+						className={classes.inputField}
 						keyboard
 						required
 						value={date}
 						onChange={this.handleChangeDate}
-						label="Datum"
+						label="Date"
 						format="MM/dd/yyyy"
-						placeholder="MM/dd/yyyy"
+						placeholder="MM/DD/YYYY"
 						mask={value => (value ? [/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/] : [])}
 						disableOpenOnEnter
 						animateYearScrolling={false}
 					/>
-				</div>
+					<TextField
+						className={classes.inputField}
+						label="Address"
+						name="address"
+						value={address}
+						onChange={this.handleChangeInput}
+						multiline
+					/>
+
+					<div className={classes.actionButtonsContainer}>
+						<Button
+							type="submit"
+							variant="contained"
+							color="secondary"
+						>
+							Save Location
+						</Button>
+						<Button
+							variant="contained"
+							color="secondary"
+						>
+							Cancel
+						</Button>
+					</div>
+				</form>
+
 			</div>
 		);
 	}
 
 }
 
-export default LocationEditPage;
+export default compose(
+	withStyles(styles),
+)(LocationEditPage);
