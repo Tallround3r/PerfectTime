3d87ca1399efce615b4f470cd2ce38dbd5815847
TimKe <tim.kertzscher@web.de>
2018-12-03 13:34:26 +0100

    added api key to gitignore

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-	apiKey: "AIzaSyDn3QPL0qKqYJiDCt2lKVGVmjDbB1sZ9NU",
-	authDomain: "perfecttime-planyourtrip.firebaseapp.com",
-	databaseURL: "https://perfecttime-planyourtrip.firebaseio.com",
-	projectId: "perfecttime-planyourtrip",
-	storageBucket: "perfecttime-planyourtrip.appspot.com",
-	messagingSenderId: "998140555209"
-};
