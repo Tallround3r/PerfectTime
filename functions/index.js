5ec73c92cf8c56ef3688ddca2f281b1840d1a486
Lukas K <l.karcher@t-online.de>
2018-12-11 18:39:51 +0100

    removed firebase functions predeploy

--- functions/index.js
+++ functions/index.js
@@ -3,10 +3,11 @@ const functions = require('firebase-functions');
 exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 	const email = user.email;
 
-	user.sendEmailVerification().then(function () {
-		console.log(`Verification email sent to ${email}`);
-	}, function (error) {
-		console.error(error);
-	});
+	functions.auth.user().sendEmailVerification()
+		.then(function () {
+			console.log(`Verification email sent to ${email}`);
+		}, function (error) {
+			console.error(error);
+		});
 });
 
