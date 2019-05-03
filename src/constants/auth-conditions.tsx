2a5f6f3adf09483f8285d1595496f651bc64122b
Lukas Karcher <l.karcher@t-online.de>
2019-05-03 14:38:58 +0200

    fixed possible null pointer

--- src/constants/auth-conditions.tsx
+++ src/constants/auth-conditions.tsx
@@ -1,5 +1,5 @@
 import {Trip} from '../types';
 
 export const AUTH_CONDITION_WITH_DRAWER = (auth: any) => !!auth;
-export const AUTH_CONDITION_TRIP = (auth: any, trip: Trip) => !!auth
-	&& (auth.uid === trip.owner || trip.members.indexOf(auth.uid) >= 0);
+export const AUTH_CONDITION_TRIP = (auth: any, trip: Trip) => !!auth && !!trip
+	&& (auth.uid === trip.owner || (!!trip.members && trip.members.indexOf(auth.uid) >= 0));
