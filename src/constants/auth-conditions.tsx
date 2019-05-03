f803636145593f8290515b036809bb7410a8124d
Lukas Karcher <l.karcher@t-online.de>
2019-05-03 10:27:48 +0200

    defined auth condition for trips

--- src/constants/auth-conditions.tsx
+++ src/constants/auth-conditions.tsx
@@ -1,1 +1,3 @@
 export const AUTH_CONDITION_WITH_DRAWER = (auth: any) => !!auth;
+export const AUTH_CONDITION_TRIP = (auth: any, trip: Trip) => !!auth
+	&& (auth.uid === trip.owner || trip.members.indexOf(auth.uid) >= 0);
