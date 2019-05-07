95b509848415d975480d2e9fb34d68aa3baf1427
Tim Ke <Tim.Kertzscher@web.de>
2019-05-07 20:50:36 +0200

    add more unit tests ; all faker implementation

--- src/unitTests/mailChange.test.ts
+++ src/unitTests/mailChange.test.ts
@@ -7,20 +7,20 @@ let randomValidEmail = faker.internet.email();
 
 describe('new email check', () => {
     it('knows that Tim.Kertzscher@web.de is valid new Email', () => {
-        expect(validateUpdateMail(randomValidEmail,'Tim.Kertzscher@web.de', randomValidPassword)).toBe(true);
+        expect(validateUpdateMail(randomValidEmail, 'Tim.Kertzscher@web.de', randomValidPassword)).toBe(true);
     });
     it('knows that mail with missing @ is incorrect new Email', () => {
         expect(validateUpdateMail(randomValidEmail, 'Tom.Tailor.web.de', randomValidPassword)).toBe(false);
     });
     it('knows that mail with missing . is incorrect new Email', () => {
-        expect(validateUpdateMail(randomValidEmail,'Tim@mailcom', randomValidPassword)).toBe(false);
+        expect(validateUpdateMail(randomValidEmail, 'Tim@mailcom', randomValidPassword)).toBe(false);
     });
     it('knows that empty String is incorrect new Email', () => {
         expect(validateUpdateMail(randomValidEmail, '', randomValidPassword)).toBe(false);
     });
 });
 
-    describe('email check', () => {
+describe('email check', () => {
     it('knows that empty String is incorrect Email', () => {
         expect(validateUpdateMail('', randomValidNewEmail, randomValidPassword)).toBe(false);
     });
---
+++ src/unitTests/randomImage.test.ts
@@ -0,0 +1,10 @@
+let faker = require('faker');
+import {getRandomImage} from '../utils/RessourceUtils'
+
+
+
+describe('image check', () => {
+    it('returns an image', () => {
+        expect(getRandomImage()).toContain('.jpg');
+    });
+});
