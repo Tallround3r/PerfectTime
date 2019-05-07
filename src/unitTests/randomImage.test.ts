30430453087455d57e81689c24a470ea9b84c1f6
Tim Ke <Tim.Kertzscher@web.de>
2019-05-07 21:01:46 +0200

    random image test considered as useless

--- src/unitTests/randomImage.test.ts
+++
@@ -1,8 +0,0 @@
-let faker = require('faker');
-import {getRandomImage} from '../utils/RessourceUtils'
-
-describe('image check', () => {
-    it('returns an image', () => {
-        expect(getRandomImage()).toContain('.jpg');
-    });
-});
