7d8914ba01933bc1eca4fd9224179d9e885d08cf
Tim Ke <Tim.Kertzscher@web.de>
2019-05-11 02:31:21 +0200

    implemented pattern for location panel

---
+++ src/components/LocationPanel.tsx
@@ -0,0 +1,120 @@
+import {createStyles, Theme, WithStyles, withStyles} from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ArrowRightIcon from '@material-ui/core/SvgIcon/SvgIcon';
+import Typography from '@material-ui/core/Typography';
+import firebase from 'firebase';
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import * as routes from '../constants/routes';
+import PictureStar from '../images/star.jpg';
+import {Location} from '../types/location';
+import ActivitiesSlider from './ActivitiesSlider';
+import {log} from "util";
+
+type Timestamp = firebase.firestore.Timestamp;
+
+const styles = (theme: Theme) => createStyles({
+	locationPanel: {
+		border: 'thin solid #000000',
+		marginTop: theme.spacing.unit * 2,
+		marginBottom: theme.spacing.unit * 2,
+		background: theme.palette.secondary.main,
+	},
+	bigColumn: {
+		flexBasis: '50%',
+		textAlign: 'center',
+	},
+	smallColumn: {
+		flexBasis: '1%',
+		textAlign: 'right',
+	},
+	borderLeft: {
+		borderLeft: 'thin solid #000000',
+		height: '100px',
+	},
+	hrLine: {
+		width: '1px',
+		height: '67px',
+	},
+	activitiesContainer: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		flexDirection: 'column',
+		width: '100%',
+	},
+	fab: {
+		position: 'absolute',
+		bottom: theme.spacing.unit * 5,
+		right: theme.spacing.unit * 5,
+	},
+});
+
+interface LocationPanelProps extends WithStyles<typeof styles> {
+	expanded: any,
+	key: string,
+	onChange: (event: any, expanded: any) => void,
+	location: Location,
+	startdate: Date | null,
+	enddate: Date | null,
+	tripId: string,
+}
+
+function LocationPanel({classes, expanded, key, onChange, location, startdate, enddate, tripId}: LocationPanelProps) {
+	return (
+		<div>
+			<ExpansionPanel
+				className={classes.locationPanel}
+				expanded={expanded === key}
+				onChange={onChange}
+			>
+				<ExpansionPanelSummary>
+					<div className={classes.smallColumn}>
+						<Avatar src={PictureStar}/>
+					</div>
+					<div className={classes.bigColumn}>
+						<Typography>{location.title}</Typography>
+					</div>
+					<div className={classes.bigColumn}>
+						<Typography>
+							Start Date:
+							{!!startdate && ` ${startdate.getDate()}.${startdate.getMonth()}.${startdate.getFullYear()}`}
+						</Typography>
+						<Typography>
+							End Date:
+							{!!enddate && ` ${enddate.getDate()}.${enddate.getMonth()}.${enddate.getFullYear()}`}
+						</Typography>
+					</div>
+					<div className={classes.bigColumn}>
+						<Typography>{location.description}</Typography>
+					</div>
+					<div className={classes.smallColumn}>
+						<NavLink exact={true} to={routes.LOCATIONS_VIEW(tripId, key)}>
+							<Avatar>
+								<ArrowRightIcon/>
+							</Avatar>
+						</NavLink>
+					</div>
+				</ExpansionPanelSummary>
+
+				<ExpansionPanelDetails>
+					<div style={{width: '100%'}}>
+							{console.log(tripId)}
+						{console.log(key)}
+						{
+							// @ts-ignore
+							<ActivitiesSlider
+								tripId={tripId}
+								locationId={key}  // TODO this key here is undefined...
+							/>
+						}
+					</div>
+				</ExpansionPanelDetails>
+			</ExpansionPanel>
+		</div>
+	);
+}
+
+export default withStyles(styles)(LocationPanel);
