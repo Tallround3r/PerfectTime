bf1ef2030f26758e7a7fba5d862529cf48f6be03
jasonmf123 <jari94@gmx.de>
2019-05-16 11:48:45 +0200

    first layout

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,24 @@
-// const functions = require('firebase-functions');
+import * as functions from "firebase";
+
+const functions = require('firebase-functions');
+
+let onUserDelete = (snap, context) => {
+	const user = snap.data();
+	const id = user.id;
+	const users = functions.firestore.
+
+	//delete all trips owned by deleted user
+
+
+	// deleted all references on deleted user in followedUser
+
+
+	// delete all references on deleted user in tripMembers
+
+
+
+}
+
 //
 // exports.sendWelcomeEmail = functions.auth.user().onCreate((user) => {
 // 	const email = user.email;
@@ -11,3 +31,6 @@
 // 		});
 // });
 //
+
+exports.deleteUser = functions.firestore.document(`$/users/{userId}`).onDelete(onUserDelete);
+
