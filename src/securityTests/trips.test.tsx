a912dbf0694232f705de86daa0585000f61db2c6
Tim Ke <Tim.Kertzscher@web.de>
2019-05-16 16:57:53 +0200

    formatted test files properly

---
+++ src/securityTests/trips.test.tsx
@@ -0,0 +1,6 @@
+// Scenario: The user should not be able to see restricted area edit trip
+//
+// Scenario: The user should not be able to see restricted area add trip
+//
+// Scenario: The user should not be able to see restricted delete edit trip
+//
