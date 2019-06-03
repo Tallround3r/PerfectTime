5ad80f924c45a1ed943f9b4995968cba810bb39f
jasonmf123 <jari94@gmx.de>
2019-06-03 15:42:12 +0200

    with delete trip

--- functions/index.js
+++ functions/index.js
@@ -16,11 +16,36 @@ let onUserDelete = (snap, context) => {
 	firestore.collection("TRIPS").get().then(snap => {
 		snap.forEach(tripDoc => {
 			const tripObj = tripDoc.data();
+			let tripDeleted = false;
+			// Search all trips owned by deleted user
 			if(tripObj.owner === id)
 			{
-				// TODO: call delete Trip
+				// trip has other members -> next member becomes the owner
+				if(tripObj.members[0] && tripObj.members[0]!==id)
+				{
+					tripObj.owner = tripObj.members[0];
+					firestore.set(tripDoc.ref, tripObj) //TODO: does ref work?
+				} else if(tripObj.members[1] && tripObj.members[1]!==id)
+				{ // in case the (deleted) owner has been registered as the first member
+					tripObj.owner = tripObj.members[1];
+					firestore.set(tripDoc.ref, tripObj) //TODO: does ref work?
+				} else if(tripObj.public) { // a public trip without owner or members stays public
+					tripObj.owner = 'public';
+					firestore.set(tripDoc.ref, tripObj) //TODO: does ref work?
+				} else { // delete trip
+						firestore.delete(tripDoc.ref, {
+							project: process.env.GCLOUD_PROJECT,
+							recursive: true,
+							yes: true,
+							token: functions.config().fb.token,
+						})
+					tripDeleted = true;
+				}
+
+				// TODO: check if it worked
 			}
-			else {
+			if(!tripDeleted) { // unnecessary if trip has been deleted
+				// delete membership of deleted user from all trips
 				const members = tripObj.members;
 				const index = members.indexOf(id);
 				if(index >= 0) {
